1c1887de6ddb18135fc9c5126febd16d285d73c7
榎本 芳昭 <yoshi.officemail15.1122@gmail.com>
2018-02-20 17:34:02 +0900

    Deviseのビューファイル生成＆追加まで
    ・コマンドによるファイル生成
    ・＆ビューファイルの差し替え（４点）
    ・scssに使用ファイルの追記

--- db/migrate/20180220075832_devise_create_users.rb
+++ db/migrate/20180220075832_devise_create_users.rb
@@ -4,7 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :name,               null: false, unique: true
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
