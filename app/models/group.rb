297139d52057f860b6ca7955139d4f145a1c6bf7
榎本 芳昭 <yoshi.officemail15.1122@gmail.com>
2018-02-23 15:16:47 +0900

    6.状況に応じて値が変動
    ・サイドバーの値をグループメッセと連動化
    ・部分テンプレートの集約化
    （vews/sharedを作成し、その配下にファイルを移動・管理）

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :messages
   validates :group_name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : "画像が投稿されています"
+    else
+      "まだメッセージはありません。"
+    end
+  end
 end
