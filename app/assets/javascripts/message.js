ed4a57ddf1ffbff8780ef98b20447f79003b5c7e
榎本 芳昭 <yoshi.officemail15.1122@gmail.com>
2018-02-27 20:28:03 +0900

    ３、４、５
    ・指定のコントローラーを呼ぶ
    （message#createへのURL設定）
    ・respond_toの使用
    ・jbuilderの作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,18 @@ $(function() {
   $(".form").submit(function(e) {
     e.preventDefault();
     console.log(this);
-    var formData = new FormData(this);
+    var formData = new FormData($("form")[0])
+    // var formData = new FormData(this);
+    var url = $(this).attr("action");
+    // var url = $(formData).attr("action");
+    console.log(formData);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
   })
 })
-
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+  json.body  @message.body
+  json.image  @message.image.url
+  json.created_at  @message.create_at
+  json.user_name  @message.user.name
