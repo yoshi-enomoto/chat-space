eb4c9a7f1e0c827dfdfcf1f0318567650992f7b1
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-03-10 00:49:50 +0900

    S3設定

--- Gemfile
+++ Gemfile
@@ -72,4 +72,5 @@ gem "font-awesome-rails"
 gem "devise"
 gem "carrierwave"
 gem "mini_magick"
+gem "fog"
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
+set :linked_files, %w{ config/secrets.yml }
 set :application, "chat-space"
 set :repo_url, "git@github.com:yoshi-enomoto/chat-space.git"
 
