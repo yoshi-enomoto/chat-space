c2fb0e15cf262902192103ce14c35bee7b11fa71
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-04 14:59:18 +0900

    画像のAWSとローカルSVとの切り分け追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,12 +3,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  # process resize_to_fit: [800, 800] 画像のリサイズ変更のため
+  process resize_to_fit: [400, 400]
 
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
+  if use_s3?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,55 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
-  }
-
-  config.fog_directory  = 'upload--tteesstt'
-  config.asset_host = 'https://s3-us-east-1.amazonaws.com/upload--tteesstt'
+# ローカルでの保存は機能しない（AWSでの保存も確認していない）
+# S3を使用しているかを判定するためのメソッド。（ローカル環境ではS3を使わないため）
+def use_s3?
+  Rails.application.secrets.aws_access_key_id && Rails.application.secrets.aws_secret_access_key && 'us-east-1' && 'upload--tteesstt'
 end
+
+## CarrierWaveの設定
+CarrierWave.configure do |config|
+  # S3の設定
+  if use_s3?
+    # config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'us-east-1'
+    }
+
+  # S3のバケットを指定。
+  config.fog_directory  = 'upload--tteesstt'
+
+  # 一般公開させて無いS3の場合は以下の設定を行う。
+  # config.fog_public     = false
+
+  # 一般公開されていない場合は以下の設定をする事で60秒間有効なURLを発行してくれる。
+  # config.fog_authenticated_url_expiration = 60
+  # CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+  config.asset_host = 'https://s3-us-east-1.amazonaws.com/upload--tteesstt'
+  end
+
+  # public配下にキャッシュができると参照されてしまうので、予め変えておく。
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+
+
+end
+
+
+
+# 元々の設定
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+#     region: 'us-east-1'
+#   }
+
+#   config.fog_directory  = 'upload--tteesstt'
+#   config.asset_host = 'https://s3-us-east-1.amazonaws.com/upload--tteesstt'
+# end
